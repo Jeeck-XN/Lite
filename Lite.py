8deeb816f729ad4aac5f991687177e519b3049e3
Jeeck X Nano <87571831+Jeeck-XN@users.noreply.github.com>
2022-02-20 17:00:18 +0700

    Add files via upload

---
+++ Lite.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python2 
+#coding=utf-8
+import os,sys
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup as parser
+import requests, os, re, bs4, sys, json, time, random, datetime
+import requests,sys,os,time,json
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor as YayanGanteng
+from datetime import datetime
+from bs4 import BeautifulSoup
+from time import sleep as jeda
+from datetime import datetime
+from calendar import monthrange
+import requests,bs4,sys,os,random,time,re,json
+import calendar
+import requests,bs4,sys,os,subprocess,time,datetime
+import requests,sys,random,re,base64,json
+from multiprocessing.pool import ThreadPool
+p = ("File+Tools+lite+seharga+50K")
+peli = ("File+Tools+lite+opensource+code+seharga+250K")
+os.system("clear")
+print("╔═════════════════════════════════════════════╗")
+print("║ NO ║ TOOLS FILE        ║ HARGA FILE         ║")
+print("╚═════════════════════════════════════════════╝")
+print("║ 01 ║ File + encrypt    ║ 50k                ║")
+print("║ 02 ║ File + Opencode   ║ 250k               ║")
+print("║ 03 ║ Cek menu Crack fb ║        - -         ║")
+print("║ 04 ║ Cek menu Bot fb   ║        - -         ║")
+print("║ 05 ║ Cek menu Crack ig ║        - -         ║")
+print("╚═════════════════════════════════════════════╝")
+lite = raw_input("\n Input :")
+if lite in ['1','01']:
+   os.system('am start https://wa.me/6281392505882?text=Assalamualaikum,+Bang+jeeck+X+Nano,+Saya+membeli:%20' + p + ' >/dev/null')
+elif lite in ['2','02']:
+     os.system('am start https://wa.me/6281392505882?text=Assalamualaikum,+Bang+jeeck+X+Nano,+Saya+membeli:%20' + peli + ' >/dev/null')
+elif lite in ['3','03']:
+     print("╔══════════════════════════════════════════════════════╗")
+     print("║ NO ║ Menu crack                 ║ SEPEK DUMP         ║")
+     print("╚══════════════════════════════════════════════════════╝")
+     print("║ 01 ║ Dump id publik massall     ║  New && Old        ║")
+     print("║ 02 ║ Dump id publik massall     ║  Old               ║")
+     print("║ 03 ║ Dump id publik massall     ║  New               ║")
+     print("║ 04 ║ Dump id publik             ║  Paling Tua        ║")
+     print("║ 05 ║ Dump id followers          ║  Old               ║")
+     print("║ 06 ║ Dump id followers          ║  New               ║")
+     print("║ 07 ║ Dump id publik             ║  New && Old        ║")
+     print("║ 08 ║ Dump id publik             ║  Old               ║")
+     print("║ 09 ║ Dump id publik             ║  New               ║")
+     print("║ 10 ║ Dump id                    ║  Publik / taman    ║")
+     print("║ 11 ║ Dump id followers          ║  New && old        ║")
+     print("║ 12 ║ Dump id Pesan              ║  100+              ║")
+     print("║ 13 ║ Dump id Group              ║  3000              ║")
+     print("║ 14 ║ Dump id reaction post      ║  1000              ║")
+     print("║ 15 ║ Dump id pencarian nama     ║  100+              ║")
+     print("╚══════════════════════════════════════════════════════╝")
+     print("╔══════════════════════════════════════════════════════╗")
+     print("║ NO ║ Metode crack               ║ SEPEK DUMP         ║")
+     print("╚══════════════════════════════════════════════════════╝")
+     print("║ 01 ║ B-Api                      ║  Gampang kena spam ║")
+     print("║ 02 ║ Mbasic                     ║  Rawan spam        ║")
+     print("║ 03 ║ Mobile                     ║  Recomended V1     ║")
+     print("║ 03 ║ Mobile                     ║  Recomended V2     ║")
+     print("╚══════════════════════════════════════════════════════╝")
+elif lite in ['4','04']:
+     print("╔══════════════════════════════════════════════════════╗")
+     print("║ NO ║ Menu bot facebook          ║  SEPEK BOT         ║")
+     print("╚══════════════════════════════════════════════════════╝")
+     print("║ 01 ║ Reaction target post       ║  Gampang kena modar║")
+     print("║ 02 ║ Reaction group post        ║  Gampang kena modar║")
+#     print("║ 03 ║ Mobile                     ║  Recomended V2     ║")
+ #    print("╚══════════════════════════════════════════════════════╝")
+elif lite in ['5','05']:
+      print "beli baru bisa ok"
+else:
+     print"asu"
+

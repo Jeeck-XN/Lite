2c19a4e9f963fededd00c710f90af36b2a30ab6b
Jeeck X Nano <87571831+Jeeck-XN@users.noreply.github.com>
2022-03-03 08:01:09 +0700

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-#GNU nano 6.0                         run.py
-if __name__ == "__main__":
-        try:
-                __import__("Lite").menu()
-        except Exception as e:
-                exit(str(e))
